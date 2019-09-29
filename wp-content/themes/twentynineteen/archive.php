e70b559bb1f075fd1d4bccdc2387cc734c9fc723
elchis <christianld_103@hotmail.es>
2019-09-29 13:31:50 -0600

    Update archive.php

--- wp-content/themes/twentynineteen/archive.php
+++ wp-content/themes/twentynineteen/archive.php
@@ -18,6 +18,17 @@ get_header();
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
+
+			<!-- Global site tag (gtag.js) - Google Analytics -->
+				<script async src="https://www.googletagmanager.com/gtag/js?id=UA-148917817-1"></script>
+				<script>
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+
+				gtag('config', 'UA-148917817-1');
+				</script>
+
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 				?>
