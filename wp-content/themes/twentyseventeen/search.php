a531b4316a5872be889d66de8211e1ebba543e6c
elchis <christianld_103@hotmail.es>
2019-09-29 13:32:24 -0600

    Update search.php

--- wp-content/themes/twentyseventeen/search.php
+++ wp-content/themes/twentyseventeen/search.php
@@ -15,6 +15,17 @@ get_header(); ?>
 <div class="wrap">
 
 	<header class="page-header">
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-148917817-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-148917817-1');
+	</script>
+
 		<?php if ( have_posts() ) : ?>
 			<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyseventeen' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 		<?php else : ?>
